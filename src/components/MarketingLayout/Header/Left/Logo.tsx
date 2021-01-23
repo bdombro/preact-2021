f1e6644208fb049524c7e8dc19755d505f9ef2bb
Brian <bdombro@gmail.com>
2021-01-23 07:15:45 -0600

    Logo i nheader

--- src/components/MarketingLayout/Header/Left/Logo.tsx
+++ src/components/MarketingLayout/Header/Left/Logo.tsx
@@ -2,5 +2,8 @@ import styles from  './Logo.module.css'
 import { h } from 'preact';
 
 export default function HeaderLogo() {
-    return <a className={styles.logo} href='/'>Stacks!</a>
+    return <a className={styles.logo} href='/'>
+        <img src='/logo192.png' />
+        <div>Stacks!</div>
+    </a>
 }
