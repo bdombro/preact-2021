24f483ac19ff14a2f2df7f55a862505a20044759
Brian <bdombro@gmail.com>
2021-01-24 16:56:26 -0600

    Sidebar uses class now

--- src/components/TenantLayout/Sidebar/Sidebar.tsx
+++ src/components/TenantLayout/Sidebar/Sidebar.tsx
@@ -1,32 +1,12 @@
 import styles from '~/components/AdminLayout/Sidebar/Sidebar.module.css'
 import { h } from 'preact';
 import lazy from '~/lib/lazy';
-import { useEffect } from 'preact/hooks';
 
 const Nav = lazy(() => import('./Nav'))
 
 
 export default function Sidebar() {
-    useEffect(listenForToggle, [])
-    
     return <div class={styles.sidebar}>
         <Nav />
     </div>
-
-    function listenForToggle() {
-        const event = '#toggle-sidebar'
-        window.addEventListener(event, toggle)
-        return () => window.removeEventListener(event, toggle)
-    }
-    function toggle(e: any) {
-        if (e) e.preventDefault()
-        const layoutElement = document.getElementById('layout')!
-        const fullWidth = getComputedStyle(layoutElement).getPropertyValue('--sidebar-width-full')
-        const miniWidth = getComputedStyle(layoutElement).getPropertyValue('--sidebar-width-mini')
-        const current = getComputedStyle(layoutElement).getPropertyValue('--sidebar-width')
-        layoutElement.style.setProperty(
-            '--sidebar-width', 
-            current === miniWidth ? fullWidth : miniWidth,
-        )
-    }
 }
--- src/components/TenantLayout/TenantLayout.tsx
+++ src/components/TenantLayout/TenantLayout.tsx
@@ -2,6 +2,7 @@ import '../AdminLayout/AdminLayout.css'
 import { h } from 'preact'
 import lazy from '../../lib/lazy'
 import { ContextProvider } from './context'
+import { useEffect, useRef } from 'preact/hooks'
 
 const BottomNav = lazy(() => import('./BottomNav'))
 const Header = lazy(() => import('./Header'))
@@ -9,9 +10,12 @@ const Sidebar = lazy(() => import('./Sidebar'))
 const SidebarRight = lazy(() => import('./SidebarRight'))
 
 export default function TenantLayout({ children }: { children: any }) {
+  const ref = useRef<HTMLDivElement>(null)
+  useEffect(listenForToggle, [])
+
   return (
     <ContextProvider>
-      <div id="layout" class="adminLayout">
+      <div class="adminLayout" ref={ref}>
         <Header />
         <Sidebar />
         <SidebarRight />
@@ -22,4 +26,13 @@ export default function TenantLayout({ children }: { children: any }) {
       </div>
     </ContextProvider>
   )
+
+  // Use event listeners instead of context, so that we don't trigger
+  // unnecessary re-renders
+  function listenForToggle() {
+    const toggle = () => ref.current.classList.toggle('miniSidebar')
+    const event = '#toggle-sidebar'
+    window.addEventListener(event, toggle)
+    return () => window.removeEventListener(event, toggle)
+  }
 }
