99fb5f62ae132426525eb0471f81fcc1a4b29c06
Brian <bdombro@gmail.com>
2021-02-21 10:10:32 -0600

    Tweaks to cms table and qs

--- src/lib/queryStrings.tsx
+++ src/lib/queryStrings.tsx
@@ -1,11 +1,18 @@
-import { nav } from './router'
-
 export default {
+	/**
+	 * parse: convenience wrapper to parse URLSearchParams into an Object
+	 * @param qs - search string, like location.search
+	 */
 	parse<T extends Record<string, any>>(qs = location.search) {
 		return Object.fromEntries((new URLSearchParams(qs) as any).entries()) as T
 	},
-	create(obj: any, {upsert = false, includeQuestionMark = true}) {
-		if (upsert) obj = Object.assign(this.parse(), obj)
+	/**
+	 * create: create a URLSearchParam string from Object
+	 * @param obj - the object
+	 * @param options - upsert: update an existing search string; includeQuestionMark: if truthy prepend with '?'
+	 */
+	create(obj: any, { upsert = false, upsertFrom = location.search, includeQuestionMark = true}) {
+		if (upsert) obj = Object.assign(this.parse(upsertFrom), obj)
 		Object.entries(obj).forEach(([k,v]) => { if(v === undefined) delete obj[k] })
 		const sp = new URLSearchParams()
 		Object.entries(obj).forEach(function setUrlSearchParams([k,v]) {
